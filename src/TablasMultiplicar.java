9ef1b2800a4ec79e381fd82ccc67e9474fa63410
costa <costa@IsmaPortatil>
2022-05-02 09:31:36 +0200

    Tablas Isma

--- src/TablasMultiplicar.java
+++ src/TablasMultiplicar.java
@@ -10,7 +10,13 @@ public class TablasMultiplicar {
 		// Crear una función que imprima las 10 tablas de 
 		// multiplicar
 		//*****************************************
-		
+			
+		for(int i = 1; i <= 10; i++) {
+			System.out.println("\n" + "La tabla del " + i + ":");
+				for(int j = 1; j <= 10; j++) {
+					System.out.println(i + " x " + j + " = " + (i*j));
+				}
+		}
 		
 	}
 	public static void saludar() {
