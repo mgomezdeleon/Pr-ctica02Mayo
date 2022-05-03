ee77b3f4e0339c42ee1f6019ad8239a85acca30a
Samu <Samu@192.168.56.1>
2022-05-03 18:31:41 +0200

    asdad

--- src/TablasMultiplicar.java
+++ src/TablasMultiplicar.java
@@ -1,5 +1,13 @@
 import java.util.Scanner;
 public class TablasMultiplicar {
 
+	public static void main(String[] args) {
+		for(int i = 1;i<11; i++) {
+			System.out.println("Tabla de multiplicar del" +i);
+			for(int e = 1; e<11; e++) {
+				System.out.println(i+" x "+e+" = "+(i*e));	
+			}	
+		}
+	}
 	
 }
