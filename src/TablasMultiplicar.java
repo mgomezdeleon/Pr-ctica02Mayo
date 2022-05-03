00ceb71f789fb3341312da18f731012567cd2fa3
Samu <Samu@192.168.56.1>
2022-05-03 18:28:33 +0200

    comit

--- src/TablasMultiplicar.java
+++ src/TablasMultiplicar.java
@@ -1,16 +1,5 @@
 import java.util.Scanner;
 public class TablasMultiplicar {
 
-	public static void main(String[] args) {
-		for(int i = 1;i<11; i++) {
-			System.out.println("Tabla de multiplicar del" +i);
-			for(int e = 1; e<11; e++) {
-				System.out.println(i+" x "+e+" = "+(i*e));	
-			}	
-		}
-	}
-	public static void saludar() {
-		// TODO Auto-generated method stub
-		System.out.println("Buenos días 1ºde Dam!!!");
-	}
+	
 }
