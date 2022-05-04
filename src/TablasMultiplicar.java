fcdb156417b749b676594f0ab12a508392f5d22e
Mateo <Mateo@DESKTOP-SEL1953>
2022-05-04 11:52:44 +0200

    Mateo Frias

--- src/TablasMultiplicar.java
+++ src/TablasMultiplicar.java
@@ -10,8 +10,15 @@ public class TablasMultiplicar {
 		// Crear una función que imprima las 10 tablas de 
 		// multiplicar
 		//*****************************************
-		
-		
+			int num=1;
+		    //bucle 1
+			for(int i=1 ; i<=10 ; i++) {
+					System.out.println(num + "X" + i + "=" + (num*i) );
+					//bucle 2
+					for(int j=1 ; j <=10 ; j++) {
+						System.out.println(i + "X" + j + "=" +(i*j) );
+					}
+			}
 	}
 	public static void saludar() {
 		// TODO Auto-generated method stub
