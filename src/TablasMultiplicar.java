a397c7b62de331d622b271c8ea453046c89cb352
Marta <Marta@DESKTOP-KGQQT70>
2022-05-04 12:28:56 +0200

    modificado por Marta

--- src/TablasMultiplicar.java
+++ src/TablasMultiplicar.java
@@ -10,6 +10,15 @@ public class TablasMultiplicar {
 		// Crear una función que imprima las 10 tablas de 
 		// multiplicar
 		//*****************************************
+			
+	        for(int i = 1; i<=10; i++){
+	        	for(int j = 0; j <=10; j++) {
+	        		System.out.println(i + " X " + j + " = " + (i*j));
+	        	}
+	                                                      
+	        }
+			
+			
 	System.out.println("hola");	
 		
 	}
