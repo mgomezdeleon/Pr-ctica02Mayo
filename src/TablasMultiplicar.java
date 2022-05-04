3be605d8f41407182d92a5095546ffebdb12aba5
ALUMNOS <ALUMNOS@P608>
2022-05-04 11:52:02 +0200

    Multiplicar angel

--- src/TablasMultiplicar.java
+++ src/TablasMultiplicar.java
@@ -2,19 +2,15 @@ import java.util.Scanner;
 public class TablasMultiplicar {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// Práctica Tablas de Multiplicar
-			saludar();
-			
-		//*****************************************
-		// Crear una función que imprima las 10 tablas de 
-		// multiplicar
-		//*****************************************
 		
+		multiplicar();
 		
-	}
-	public static void saludar() {
-		// TODO Auto-generated method stub
-		System.out.println("Buenos días 1ºde Dam!!!");
+	}public static void multiplicar() {
+		for(int i = 1; i <=10;i++) {
+			System.out.println("Tabla del: " + i);
+			for (int j = 1; j <=10;j++) {
+				System.out.println(j*i);
+			}
+		}
 	}
 }
