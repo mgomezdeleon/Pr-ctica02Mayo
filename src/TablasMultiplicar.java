5d02d725c9503010b89e0d770bd34c32e29ef08a
Lara <Lara@LAPTOP-L23FN667>
2022-05-09 09:24:47 +0200

    Rama de Lara :)

--- src/TablasMultiplicar.java
+++ src/TablasMultiplicar.java
@@ -17,15 +17,21 @@ public class TablasMultiplicar {
 	}
 	
 	public static void multiplicar() {
+		
 		for (int i = 0; i < 10; i++) {
 			System.out.println("Tabla del " + i);
 			for (int j = 0; j < 10; j++) {
 				System.out.println(i +  "*" + j + " = " + i*j);
 			}
+		
 		}
+	
 	}
+	
 	public static void saludar() {
 		// TODO Auto-generated method stub
 		System.out.println("Buenos d�as 1�de Dam!!!");
 	}
+	
+	
 }
